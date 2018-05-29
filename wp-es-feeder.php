155be828810a59bbb16285d938ca13c6f1205a17
Scott Gustas <scottius@gmail.com>
2018-05-29 10:59:40 -0400

    Rev to 2.1.0

--- wp-es-feeder.php
+++ wp-es-feeder.php
@@ -7,9 +7,9 @@
  * @wordpress-plugin
  * Plugin Name:       WP Elasticsearch Feeder
  * Description:       Creates REST api endpoints for each post type and indexes them into Elasticsearch.
- * Version:           1.0.0
- * Author:            Max Orelus
- * Author URI:        http://github.com/MaxOrelus
+ * Version:           2.1.0
+ * Author:            IIP Design
+ * Author URI:        https://github.com/IIP-Design
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-es-feeder
