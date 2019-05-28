6802a46579bd8f6c8f1d193df3be33ca95bdc634
Scott Gustas <scottius@gmail.com>
2019-05-28 15:27:33 -0400

    debug owners

--- admin/partials/wp-es-feeder-owner-display.php
+++ admin/partials/wp-es-feeder-owner-display.php
@@ -6,6 +6,6 @@ $owners = $cdp_owner_helper->get_owners();
 <select id="cdp-owner" data-placeholder="Select Owner" name="cdp_owner" title="Owner" style="width: 100%">
     <option value="<?=$sitename?>" <?=($selected === $sitename ? 'selected="selected"' : '')?>><?=$sitename?></option>
     <?php foreach ($owners as $owner): ?>
-        <option value="<?=$owner->name?>" <?=($selected === $owner->name ? 'selected="selected"' : '')?>><?=$owner->name?></option>
+        <option value="<?=$owner?>" <?=($selected === $owner ? 'selected="selected"' : '')?>><?=$owner?></option>
     <?php endforeach; ?>
 </select>
