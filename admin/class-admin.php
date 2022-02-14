c0d3e4808e278569acb7266d46d79723322a123a
mrewers <hello@marek.dev>
2022-02-14 12:52:30 -0500

    refactor: rename nonce to make clear it pertains to the feeder plugin

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -184,8 +184,8 @@ class Admin {
       $this->handle_settings,
       'gpalabFeederSettings',
       array(
-        'nonce'      => wp_create_nonce( 'gpalab-feeder-nonce' ),
-        'syncTotals' => $sync,
+        'feederNonce' => wp_create_nonce( 'gpalab-feeder-nonce' ),
+        'syncTotals'  => $sync,
       )
     );
   }
